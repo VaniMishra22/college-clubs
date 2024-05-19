c37ccca5812e56bc9cdbeddb66a0b245d77dd933
VaniMishra22 <vanimishra22@gmail.com>
2024-05-19 14:39:27 +0530

    clubs

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -18,8 +18,6 @@ import {
   rem,
   useMantineTheme,
   Title,
-  Menu,
-  Avatar,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import {
@@ -30,137 +28,16 @@ import {
   IconFingerprint,
   IconCoin,
   IconChevronDown,
-  IconUser,
-  IconLogout,
-  IconUsersGroup,
 } from '@tabler/icons-react';
 import classes from './navbar.module.css';
 import { ActionToggle } from './ActionToggle';
-import useAppContext from '@/context/AppContext';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import cx from 'clsx';
-
-const mockdata = [
-  {
-    icon: IconCode,
-    title: 'Open source',
-    description: 'This Pokémon’s cry is very loud and distracting',
-  },
-  {
-    icon: IconCoin,
-    title: 'Free for everyone',
-    description: 'The fluid of Smeargle’s tail secretions changes',
-  },
-  {
-    icon: IconBook,
-    title: 'Documentation',
-    description: 'Yanma is capable of seeing 360 degrees without',
-  },
-  {
-    icon: IconFingerprint,
-    title: 'Security',
-    description: 'The shell’s rounded shape and the grooves on its.',
-  },
-  {
-    icon: IconChartPie3,
-    title: 'Analytics',
-    description: 'This Pokémon uses its flying ability to quickly chase',
-  },
-  {
-    icon: IconNotification,
-    title: 'Notifications',
-    description: 'Combusken battles with the intensely hot flames it spews',
-  },
-];
+import { Feature, FeaturesGrid } from './FeaturesGrid';
+import Link from 'next/link';
 
 export default function Navbar() {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
   const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
   const theme = useMantineTheme();
-
-  const router = useRouter();
-
-  const { loggedIn, currentUser, logout } = useAppContext();
-  const [userMenuOpened, setUserMenuOpened] = useState(false);
-
-  const links = mockdata.map((item) => (
-    <UnstyledButton className={classes.subLink} key={item.title}>
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" c="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </UnstyledButton>
-  ));
-
-  const showLoginOptions = () => {
-    if (loggedIn) {
-      return <Menu
-        width={260}
-        position="bottom-end"
-        transitionProps={{ transition: 'pop-top-right' }}
-        onClose={() => setUserMenuOpened(false)}
-        onOpen={() => setUserMenuOpened(true)}
-        withinPortal
-      >
-        <Menu.Target>
-          <UnstyledButton
-            className={cx(classes.user, { [classes.userActive]: userMenuOpened })}
-          >
-            <Group gap={7}>
-              <Avatar src={currentUser.avatar} alt={currentUser.name} radius="xl" size={20} />
-              <Text fw={500} size="sm" lh={1} mr={3}>
-                {currentUser.name}
-              </Text>
-              <IconChevronDown style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
-            </Group>
-          </UnstyledButton>
-        </Menu.Target>
-        <Menu.Dropdown>
-
-          <Menu.Item
-            leftSection={
-              <IconUser style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-            }
-          >
-            Profile
-          </Menu.Item>
-          <Menu.Item
-          onClick={() => router.push('/club/chat')}
-            leftSection={
-              <IconUsersGroup style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-            }
-          >
-            View Clubs
-          </Menu.Item>
-          <Menu.Item
-            color='red'
-            onClick={logout}
-            leftSection={
-              <IconLogout style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-            }
-          >
-            Logout
-          </Menu.Item>
-        </Menu.Dropdown>
-      </Menu>
-    } else {
-      return <Group visibleFrom="sm">
-        <ActionToggle></ActionToggle>
-        <Button variant='filled' onClick={e => router.push('/login')}>Log in</Button>
-        <Button variant='filled' onClick={e => router.push('/signUp')}>Sign up</Button>
-      </Group>
-    }
-  }
 
   return (
     <Box>
@@ -169,54 +46,15 @@ export default function Navbar() {
           {/* <MantineLogo size={30} /> */}
           <Title order={3}>College Clubs</Title>
 
-          <Group h="100%" gap={0} visibleFrom="sm">
             <a href="/" className={classes.link}>
               Home
             </a>
-            <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-              <HoverCard.Target>
-                <a href="#" className={classes.link}>
-                  <Center inline>
-                    <Box component="span" mr={5}>
-                      About
-                    </Box>
-                    <IconChevronDown
-                      style={{ width: rem(16), height: rem(16) }}
-                      color={theme.colors.blue[6]}
-                    />
-                  </Center>
-                </a>
-              </HoverCard.Target>
-
-              <HoverCard.Dropdown style={{ overflow: 'hidden' }}>
-                <Group justify="space-between" px="md">
-                  <Text fw={500}>Features</Text>
-                  <Anchor href="#" fz="xs">
-                    Clubs
-                  </Anchor>
-                </Group>
 
                 <Divider my="sm" />
 
                 <SimpleGrid cols={2} spacing={0}>
-                  {links}
                 </SimpleGrid>
 
-                <div className={classes.dropdownFooter}>
-                  <Group justify="space-between">
-                    <div>
-                      <Text fw={500} fz="sm">
-                        Get started
-                      </Text>
-                      <Text size="xs" c="dimmed">
-                        Their food sources have decreased, and their numbers
-                      </Text>
-                    </div>
-                    <Button variant="default">Get started</Button>
-                  </Group>
-                </div>
-              </HoverCard.Dropdown>
-            </HoverCard>
             <a href="#" className={classes.link}>
               Learn
             </a>
@@ -229,11 +67,12 @@ export default function Navbar() {
             <a href="/faq" className={classes.link}>
               FAQ's
             </a>
-          </Group>
 
-          {
-            showLoginOptions()
-          }
+          <Group visibleFrom="sm">
+            <ActionToggle></ActionToggle>
+            <button variant="filled" href='/login'>Log in</button>
+            <button variant="filled" href='/signUp'>Sign up</button>
+          </Group>
 
           <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
         </Group>
@@ -254,18 +93,7 @@ export default function Navbar() {
           <a href="#" className={classes.link}>
             Home
           </a>
-          <UnstyledButton className={classes.link} onClick={toggleLinks}>
-            <Center inline>
-              <Box component="span" mr={5}>
-                About
-              </Box>
-              <IconChevronDown
-                style={{ width: rem(16), height: rem(16) }}
-                color={theme.colors.blue[6]}
-              />
-            </Center>
-          </UnstyledButton>
-          <Collapse in={linksOpened}>{links}</Collapse>
+
           <a href="#" className={classes.link}>
             Learn
           </a>
@@ -288,5 +116,4 @@ export default function Navbar() {
         </ScrollArea>
       </Drawer>
     </Box>
-  );
-}
+  );}
