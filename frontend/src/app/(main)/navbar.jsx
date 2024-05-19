509744ca22bf5688579c87dd8100ecd3d372af48
m-triple-m <triplem656@gmail.com>
2024-05-19 14:47:55 +0530

    ..

--- frontend/src/app/(main)/navbar.jsx
+++ frontend/src/app/(main)/navbar.jsx
@@ -135,7 +135,7 @@ export default function Navbar() {
             Profile
           </Menu.Item>
           <Menu.Item
-          onClick={() => router.push('/club/chat')}
+            onClick={() => router.push('/club/chat')}
             leftSection={
               <IconUsersGroup style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
             }
