b1f121d8201f2c7e43debe5ded51936e54619663
m-triple-m <triplem656@gmail.com>
2024-06-15 10:40:53 +0530

    ..

--- frontend/src/context/ClubContext.jsx
+++ frontend/src/context/ClubContext.jsx
@@ -6,27 +6,27 @@ const ClubContext = createContext();
 
 export const ClubProvider = ({ children }) => {
 
-    const { id } = useParams();
-    // console.log(id);
-    const [selClub, setSelClub] = useState(null);
+  const { id } = useParams();
+  // console.log(id);
+  const [selClub, setSelClub] = useState(null);
 
-    const fetchClubDetails = () => {
-        fetch("http://localhost:5000/club/getbyid/" + id)
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => {
-            console.log(data);
-            setSelClub(data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
+  const fetchClubDetails = () => {
+    fetch("http://localhost:5000/club/getbyid/" + id)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setSelClub(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    return <ClubContext.Provider value={{ selClub, setSelClub, fetchClubDetails }}>
-        {children}
-    </ClubContext.Provider>;
+  return <ClubContext.Provider value={{ selClub, setSelClub, fetchClubDetails }}>
+    {children}
+  </ClubContext.Provider>;
 }
 
 const useClubContext = () => useContext(ClubContext);
